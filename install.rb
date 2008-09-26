8522c99ff3d499f7044904f3ef3808d2f8201d90
PullMonkey <charlie@pullmonkey.com>
2008-09-26 19:43:38 +0000

    added install text

--- install.rb
+++ install.rb
@@ -1,1 +1,7 @@
 # Install hook code here
+require 'ftools'
+
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
+puts "\n\n======================================================"
+puts "\n\nMoving all required javascript and css files to your public directory..."
+`rake simple_calendar:install`
