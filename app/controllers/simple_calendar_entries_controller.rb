e9c145347eb442f53d1dc249acf52acfd4a316b0
PullMonkey <charlie@pullmonkey.com>
2008-10-29 17:03:35 +0000

    fixed @calendar_path to work for return_to and prefix and used it trhoughout the rest of the controller

--- app/controllers/simple_calendar_entries_controller.rb
+++ app/controllers/simple_calendar_entries_controller.rb
@@ -1,17 +1,23 @@
 class SimpleCalendarEntriesController < ApplicationController
+  # For Rails version 2.0 to 2.1
+  #self.view_paths << File.join(File.dirname(__FILE__), '..', 'views')
+  # For Rails versions less than 2.0
+  #self.template_root = File.join(File.dirname(__FILE__), '..', 'views')
+
   before_filter :set_simple_calendar_defaults
 
   def new
     @mode = params[:mode] ? params[:mode] : "month"
-    year, month, day = params[:id].split("_")
-    @date = Date.civil(year.to_i, month.to_i, day.to_i).to_time.utc
-    logger.error "*************************************************************************************"
-    logger.error "Date: #{@date}"
+    @date = params[:id].to_time.utc
+    if !@admin
+      redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + "?date=#{@date.to_date}"
+      return
+    end
     if !params[:hour].nil?
       @hour = params[:hour].to_i
-      @date -= 6.hours
       @date += @hour.hours
-      logger.error "Date after: #{@date.to_time}"
+    else
+      @date += 6.hours
     end
     @simple_calendar_entry = @simple_calendar.simple_calendar_entries.new(:start_time => @date.to_time, :end_time => @date.to_time + 1.hour)
     render :partial => 'form'
@@ -19,14 +25,16 @@ class SimpleCalendarEntriesController < ApplicationController
 
   def create
     @mode = params[:simple_calendar_entry][:mode]
+    if !@admin
+      redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + "?date=#{@date.to_date}"
+      return
+    end
     params[:simple_calendar_entry].delete(:mode)
     @simple_calendar_entry = @simple_calendar.simple_calendar_entries.new(params[:simple_calendar_entry])
     @simple_calendar_entry.save ? 
       flash[:notice] = "Calendar Entry Saved" : 
       flash[:error] = "Calendar Entry Could Not Be Saved"
-    logger.error "calendar_path: " + @calendar_path.to_s
-    #redirect_to @calendar_path, :date => (@simple_calendar_entry ? "#{@simple_calendar_entry.start_time.to_date}" : nil)
-    redirect_to :controller => "simple_calendar", :action => @mode == "day" ? "day_view" : "month_view", :date => (@simple_calendar_entry ? "#{@simple_calendar_entry.start_time.to_date}" : nil)
+    redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + (@simple_calendar_entry ? "?date=#{@simple_calendar_entry.start_time.to_date}" : "")
   end
 
   def show
@@ -38,6 +46,10 @@ class SimpleCalendarEntriesController < ApplicationController
   def edit
     @mode = params[:mode] ? params[:mode] : "month"
     @simple_calendar_entry = @simple_calendar.simple_calendar_entries.find(params[:id])
+    if !@admin
+      redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + (@simple_calendar_entry ? "?date=#{@simple_calendar_entry.start_time.to_date}" : "")
+      return
+    end
     render :partial => 'form'
   end
 
@@ -45,28 +57,36 @@ class SimpleCalendarEntriesController < ApplicationController
     @mode = params[:simple_calendar_entry][:mode]
     params[:simple_calendar_entry].delete(:mode)
     @simple_calendar_entry = @simple_calendar.simple_calendar_entries.find(params[:id])
+    if !@admin
+      redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + (@simple_calendar_entry ? "?date=#{@simple_calendar_entry.start_time.to_date}" : "")
+      return
+    end
     @simple_calendar_entry.update_attributes(params[:simple_calendar_entry]) ? 
       flash[:notice] = "Calendar Entry Updated" : 
       flash[:error] = "Calendar Entry Could not be Updated"
-    #redirect_to @calendar_path
-    redirect_to :controller => "simple_calendar", :action => @mode == "day" ? "day_view" : "month_view", :date => "#{@simple_calendar_entry.start_time.to_date}"
+    redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + (@simple_calendar_entry ? "?date=#{@simple_calendar_entry.start_time.to_date}" : "")
   end
 
   def delete
     @simple_calendar_entry = @simple_calendar.simple_calendar_entries.find(params[:id])
-    @confirm = "Are you sure you want to delete this entry"
-    @simple_calendar_entry.destroy ?
-      flash[:notice] = "Event Deleted" :
-      flash[:error] = "Event Could not be Deleted!"
-    #redirect_to @calendar_path
-    redirect_to :controller => "simple_calendar", :action => "month_view", :date => "#{@simple_calendar_entry.start_time.to_date}"
+    if @admin
+      @confirm = "Are you sure you want to delete this entry"
+      @simple_calendar_entry.destroy ?
+        flash[:notice] = "Event Deleted" :
+        flash[:error] = "Event Could not be Deleted!"
+    else
+      flash[:error] = "You are not an admin"
+    end
+    redirect_to "#{@calendar_path}/simple_calendar/" + (@mode == "day" ? "day_view" : "month_view") + (@simple_calendar_entry ? "?date=#{@simple_calendar_entry.start_time.to_date}" : "")
   end 
 
 private
 
   def set_simple_calendar_defaults
     @simple_calendar = SimpleCalendar.find_or_create_by_name(session[:simple_calendar_name])
-    #@calendar_path = session[:simple_calendar_path]
-    @calendar_path = home_path 
+    @calendar_path = ""
+    @calendar_path = session[:simple_calendar_prefix] if not session[:simple_calendar_prefix].empty?
+    @calendar_path = session[:simple_calendar_path] if not session[:simple_calendar_path].empty?
+    @admin = session[:simple_calendar_admin]
   end
 end
