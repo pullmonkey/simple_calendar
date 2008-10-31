c98a72b67f97368ced5e5d36b52e5a6a817f0a48
PullMonkey <charlie@pullmonkey.com>
2008-10-31 16:35:39 +0000

    added instructions for the plugin

--- install.rb
+++ install.rb
@@ -2,6 +2,6 @@
 require 'ftools'
 
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
-puts "\n\n======================================================"
-puts "\n\nMoving all required javascript and css files to your public directory..."
+puts "\n\n====================================================================="
+puts "\n\nMoving all required javascript, css files, and images to your public directory..."
 `rake simple_calendar:install`
