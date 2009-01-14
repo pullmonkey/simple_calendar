0a1f50ded44f60649bcb94fff6b8342c0b74a08b
PullMonkey <charlie@pullmonkey.com>
2009-01-14 19:38:11 +0000

    simplified model and removed unused code

--- app/models/simple_calendar_entry.rb
+++ app/models/simple_calendar_entry.rb
@@ -4,19 +4,19 @@ class SimpleCalendarEntry < ActiveRecord::Base
   validates_presence_of :name, :message => "^You must enter a name first"
   validate :end_time_after_start_time
 
-  def sibling_entries(month, year)
-    self.simple_calendar.simple_calendar_entries.
-         all_by_month_and_year(month, year).
-         group_by(&:date).
-         sort!{|a,b| a.start_time <=> b.start_time}
-  end
-
   named_scope :all_by_month_and_year,
               lambda{|month, year| {
                 :conditions => ["start_time >= ? and start_time <= ?", 
                                 Date.civil(year, month, 1),
                                 Date.civil(month == 12 ? year + 1 : year, month == 12 ? 1 : month + 1, 1)] 
               }}
+
+  def sibling_entries(month, year)
+    entries = self.simple_calendar.simple_calendar_entries.
+         all_by_month_and_year(month, year).
+         #sort{|a,b| a.start_time <=> b.start_time}.
+         group_by(&:date)
+  end
 
   def end_time_after_start_time
     if end_time < start_time
@@ -25,8 +25,6 @@ class SimpleCalendarEntry < ActiveRecord::Base
   end
 
   def date_time
-    logger.error "start time: #{start_time.to_time}"
-    logger.error "end time: #{end_time.to_time}"
     if start_time.to_date == end_time.to_date
       "#{start_time.to_time.strftime('%A, %B %d, %Y  %I:%M %p')} - #{end_time.to_time.strftime('%I:%M %p')}"
     else
@@ -42,38 +40,38 @@ class SimpleCalendarEntry < ActiveRecord::Base
     return false
   end
 
-  def all_touching(entries)
-    touching_entries = entries.select{|i| i.touching?(self)} - [self]
-    #logger.error "*********************************************************************************"
-    #logger.error "entry being tested: " + self.name
-    #logger.error "\ntouching_entries at start: " + touching_entries.map{|e| e.name}.inspect
-    if touching_entries and touching_entries.size > 1
-      touching_entries.each do |entry|
-        if touching_entries and touching_entries.size > 1
-          not_touching = true
-          touching_entries.each do |blah|
-            if !(entry == blah)
-              not_touching = false if blah.touching?(entry)
-            end
-          end
-        end
-        #logger.error "\nnot touching: #{not_touching}"
-        touching_entries = touching_entries - [entry] if not_touching
-      end
-    end
-    #logger.error "\ntouching_entries: " + touching_entries.map{|e| e.name}.inspect
-    #logger.error "*********************************************************************************"
-    return touching_entries + [self]
-  end
+#  def all_touching(entries)
+#    touching_entries = entries.select{|i| i.touching?(self)} - [self]
+#    #logger.error "*********************************************************************************"
+#    #logger.error "entry being tested: " + self.name
+#    #logger.error "\ntouching_entries at start: " + touching_entries.map{|e| e.name}.inspect
+#    if touching_entries and touching_entries.size > 1
+#      touching_entries.each do |entry|
+#        if touching_entries and touching_entries.size > 1
+#          not_touching = true
+#          touching_entries.each do |blah|
+#            if !(entry == blah)
+#              not_touching = false if blah.touching?(entry)
+#            end
+#          end
+#        end
+#        #logger.error "\nnot touching: #{not_touching}"
+#        touching_entries = touching_entries - [entry] if not_touching
+#      end
+#    end
+#    #logger.error "\ntouching_entries: " + touching_entries.map{|e| e.name}.inspect
+#    #logger.error "*********************************************************************************"
+#    return touching_entries + [self]
+#  end
 
   def all_touching_max_and_min(entries)
-    logger.error "Entries in all_touching_max_and_min: " + entries.inspect
     touching_entries = entries.select{|i| i.touching?(self)}
+    minute_increment = 15
     lcv = 0
     max = [] 
     min = touching_entries
-    while((self.start_time + lcv * 15) < self.end_time)
-      time = self.start_time + (lcv * 15).minutes
+    while((self.start_time + lcv * minute_increment) < self.end_time)
+      time = self.start_time + (lcv * minute_increment).minutes
       num = []
       touching_entries.each do |entry|
         num << entry if time.between?(entry.start_time, entry.end_time)
@@ -94,40 +92,28 @@ class SimpleCalendarEntry < ActiveRecord::Base
   end
 
   def width(day, month, year)
-    min, max = self.all_touching_max_and_min(sibling_entries(month, year)[Date.civil(year, month, day)])
+    entries = sibling_entries(month, year)[Date.civil(year, month, day)]
+    min, max = self.all_touching_max_and_min(entries)
     (90 / max.size)
   end
 
   def left(day, month, year)
-    min, max = self.all_touching_max_and_min(sibling_entries(month, year)[Date.civil(year, month, day)])
+    entries = sibling_entries(month, year)[Date.civil(year, month, day)]
+    min, max = self.all_touching_max_and_min(entries)
     left = 5
     #index = sibling_entries(month, year)[Date.civil(year, month, day)].index(self)
+    logger.error "*****************************************************************************"
+    logger.error "SELF: #{self.inspect}"
+    logger.error "MAX ARRAY: " + max.map{|e| [e.id, e.start_time]}.join(", ") + "\n\n"
     index = max.index(self)
     left += self.width(day, month, year) * index if index
     left += index
     return left
   end
 
-#  def all_touching_ids(entries)
-#    t_entries = self.all_touching(entries)
-#    t_entries.delete(self)
-#    return t_entries.map{|e| e.id}
-#  end
-
   def total_time_minutes
     (end_time - start_time) / 60
   end
-
-#  def span
-#    time = end_time.minus_with_duration(start_time)
-#    time = time.to_i / 60
-#    if (time / 30.0).to_f > (time / 30).to_i 
-#      s = (time / 30).to_i + 1
-#    else
-#      s = time / 30
-#    end
-#    return s
-#  end
 
   def date
     start_time.to_date
